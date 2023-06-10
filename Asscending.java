f164f590774b442bcc09dcddd66471d1452fbe95
Jyoti Narayan Mishra <jyotinarayanmishra750@gmail.com>
2023-06-10 10:28:15 +0530

    modified3

---
+++ Asscending.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Asscending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Asscending order: ");
+        for (int x : arr) {
+            System.out.println(x + "\t");
+        }
+    }
+}
